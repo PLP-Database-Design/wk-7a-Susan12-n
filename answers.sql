23bcc4240dd6cfc1c13d549e1d79d3bda4e82264
Susan12-n <ngesasuzan@gmail.com>
2025-05-09 13:37:12 +0300

    normal forms in sql

---
+++ answers.sql
@@ -0,0 +1,42 @@
+CREATE TABLE ProductDetail (
+  OrderID INT,
+  CustomerName VARCHAR(100),
+  Products VARCHAR(255)
+);
+
+INSERT INTO productDetail (orderId, customerName, products) VALUES
+(101, 'John Doe', 'Laptop'),
+(101, 'John Doe', 'Mouse'),
+(102, 'Jane Smith', 'Tablet'),
+(102, 'Jane Smith', 'Keyboard'),
+(102, 'Jane Smith', 'Mouse'),
+(103, 'Emily Clark', 'Phone');
+
+
+--spliting the table into orders table and orderetails table 
+CREATE TABLE Orders (
+  OrderID INT PRIMARY KEY,
+  CustomerName VARCHAR(100)
+);
+
+INSERT INTO Orders (OrderID, CustomerName) VALUES
+(101, 'John Doe'),
+(102, 'Jane Smith'),
+(103, 'Emily Clark');
+
+CREATE TABLE OrderDetails (
+  OrderID INT,
+  Product VARCHAR(50),
+  Quantity INT,
+  PRIMARY KEY (OrderID, Product),
+  FOREIGN KEY (OrderID) REFERENCES Orders(OrderID)
+);
+
+INSERT INTO OrderDetails (OrderID, Product, Quantity) VALUES
+(101, 'Laptop', 2),
+(101, 'Mouse', 1),
+(102, 'Tablet', 3),
+(102, 'Keyboard', 1),
+(102, 'Mouse', 2),
+(103, 'Phone', 1);
+
